57cbb16470a2779a98e5a74cffd5098368f31ea5
Adrian Matejov <ado.matejov@gmail.com>
2019-10-03 10:48:37 +0200

    update: parsing livetable
    Signed-off-by: Adrian Matejov <ado.matejov@gmail.com>

--- index.js
+++ index.js
@@ -103,3 +103,147 @@ exports.get_routes = function(options) {
         })   
     });
 }
+
+
+exports.get_livetable = function(stop_id) {
+    var req1 = {
+        url: 'https://imhd.sk/rt/sio2/',
+        qs: {
+            'EIO': 3,
+            'transport': 'polling'
+        },
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0'
+        }
+    }
+
+    var req2 = {
+        url: 'https://imhd.sk/rt/sio2/',
+        body: '26:42[\"tabStart\",[' + stop_id + ',[\"*\"]]]',
+        qs: {
+            'EIO': 3,
+            'transport': 'polling',
+            't': 'Mremom1',
+            'sid': ''
+        },
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0',
+            'Cookie': ''
+        }
+    }
+
+    var req3 = {
+        url: 'https://imhd.sk/rt/sio2/',
+        qs: {
+            'EIO': 3,
+            'transport': 'polling',
+            't': 'Mremom1',
+            'sid': ''
+        },
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0',
+            'Cookie': ''
+        }
+    }
+
+    var req_mapping = {
+        url: 'https://imhd.sk/ba/api/sk/cp',
+        qs: {
+            'op': 'gsn',
+            'id': ''
+        },
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0',
+            'Cookie': ''
+        }
+    }
+
+    return new Promise(function(resolve, reject) {
+
+    request.get(req1, function(err, response, body) {
+        body = JSON.parse(body.replace(/^.*\{/g, '\{'));
+
+        var sid = body.sid;
+        req2.qs['sid'] = sid;
+        req3.qs['sid'] = sid;
+
+        var sid_cookie = request.cookie('io=' + sid);
+        req2.headers['Cookie'] = sid_cookie;
+        req3.headers['Cookie'] = sid_cookie;
+        req_mapping.headers['Cookie'] = sid_cookie;
+
+        request.post(req2, function(err2, response2, body2) {
+            request.get(req3, function(err3, response3, body3) {
+                var tabs = JSON.parse(body3.replace(/^.*tabs\",/, '')
+                                           .replace(/\]$/, ''));
+
+                var ids = [];
+                Object.keys(tabs).forEach(function(key) {
+                    var table_id = tabs[key].zastavka;
+                    if (!(table_id in ids))
+                        ids.push(table_id);
+
+                    for (var i=0; i < tabs[key].tab.length; i++) {
+                        var line = tabs[key].tab[i];
+
+                        var finish_id = line.cielZastavka;
+                        if (!(finish_id in ids))
+                            ids.push(finish_id);
+
+                        if ('lastZ' in line && line.lastZ != 0)
+                            ids.push(line.lastZ);
+                    }
+                });
+
+                req_mapping.qs['id'] = ids.join();
+
+                /* Get names of destinations */
+                request.get(req_mapping, function(e, r, b) {
+                    var mapping = JSON.parse(b).sn;
+
+                    var out = {}
+
+                    /* Remap tabs */
+                    Object.keys(tabs).forEach(function(key) {
+                        var t = tabs[key];
+                        out[key] = {
+                            stop_id: t.zastavka,
+                            stop_name: mapping[t.zastavka],
+                            platform: t.nastupiste,
+                            timestamp: t.timestamp,
+                            lines: []
+                        };
+
+                        t.tab.forEach(function(line) {
+                            var delay = 0;
+                            if ('casDelta' in line)
+                                delay = line.casDelta;
+
+                            var last_stop_id = -1;
+                            var last_stop_name = ''
+                            if ('lastZ' in line) {
+                                last_stop_id = line.lastZ;
+                                if (last_stop_id != 0)
+                                    last_stop_name = mapping[last_stop_id];
+                            }
+
+                            out[key].lines.push({
+                                line: line.linka,
+                                vehicle: line.issi,
+                                destination_id: line.cielZastavka,
+                                destination_name: mapping[line.cielZastavka],
+                                time: line.cas,
+                                type: line.typ,
+                                last_stop_id: last_stop_id,
+                                last_stop_name: last_stop_name
+                            });
+                        });
+                    });
+                    return resolve(out);
+                });
+            });
+        });
+    });
+
+    });
+}
